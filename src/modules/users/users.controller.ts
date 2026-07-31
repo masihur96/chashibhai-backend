f7692b48d03b53a5c882636ff1a3ba3187129871
masihur Rohman <masihur@evidentbd.com>
2026-07-31 16:01:08 +0600

    Config

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    // This will only run if validation passes
+    return {
+      message: 'Validation passed successfully!',
+      data: createUserDto,
+    };
+  }
+}
