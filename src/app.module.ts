7434027293b5c4b8ac667a0808875aac53b5e7e3
masihur <masihur96@gmail.com>
2026-08-03 00:12:53 +0600

    Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
